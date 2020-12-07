2f0588a1dc4246079602ba033ad8b02863cb6226
murongxixi <murongxixi@gmail.com>
2020-12-07 09:11:22 +0800

    update

--- _book/upload.sh
+++ _book/upload.sh
@@ -1,4 +1,4 @@
 gitbook build
-git add _book/ font/ img/ node_modules/gitbook-plugin-mathjax/ posts/ styles/ book.json README.md SUMMARY.md upload.sh
+git add _book font img node_modules/gitbook-plugin-mathjax posts styles book.json README.md SUMMARY.md upload.sh
 git commit -m "$1"
 git push origin master
